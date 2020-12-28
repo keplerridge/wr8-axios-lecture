210e0e5b36703aa2f0a8e2f7682908334af58dc4
keplerridge <kepler.ridge@gmail.com>
2020-12-28 11:35:26 -0700

    morning lecture code along

--- src/Componenets/Collection.js
+++ src/Componenets/Collection.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Card from './Card';
+import axios from 'axios'
 
 class Collection extends Component {
     constructor(){
@@ -9,9 +10,21 @@ class Collection extends Component {
         }
     }
 
+    componentDidMount(){
+        this.getCards()
+    }
+
+    getCards = () => {
+    axios
+        .get('https://api.pokemontcg.io/v1/cards')
+        .then(res => this.setState({cards: res.data.cards}))
+        .catch(err => alert('Could not find Pokemon'))
+    }
+
     render(){
+        console.log(this.state.cards)
         const mappedCards = this.state.cards.map( card => {
-            return <Card/>
+            return <Card key={card.id} name={card.name} img={card.imageUrl}/>
         })
         return(
             <div className="collection">
